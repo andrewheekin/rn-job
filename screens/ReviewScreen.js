59b1cac9d9690f45ee4db13f0d39c52f05aed01f
Andrew Heekin <andrewheekin@gmail.com>
2018-02-28 22:01:47 -0500

    navigation options class property

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends React.Component {
   // reserved class level property that react-navigation uses
-  static navigationOptions = {
-    
-  };
+  static navigationOptions = ({ navigation }) => ({
+    title: 'Review Jobs',
+    headerRight: (
+      <Button
+        title="Settings"
+        backgroundColor="rgba(0, 0, 0, 0)"
+        color="rgba(0, 122, 255, 1)"
+        onPress={() => navigation.navigate('settings')}
+      >
+        Go Right
+      </Button>
+    ),
+  });
 
   render() {
     return (
